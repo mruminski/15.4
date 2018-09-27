afdeddb95618e3be20124f5ab12b83f0c156e812
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-27 06:09:45 +0200

    Task 4

--- script-compiled.js
+++ script-compiled.js
@@ -33,3 +33,6 @@ function average() {
 console.log(average(1));
 console.log(average(1, 3));
 console.log(average(1, 3, 6, 6));
+
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+console.log(average.apply(undefined, grades));
--- script.js
+++ script.js
@@ -23,3 +23,6 @@ function average(...args) {
 console.log(average(1));
 console.log(average(1,3));
 console.log(average(1,3,6,6));
+
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+console.log(average(...grades));
